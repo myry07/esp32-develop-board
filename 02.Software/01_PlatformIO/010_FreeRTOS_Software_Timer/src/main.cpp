cf335b2517a9526798e2501e109b13222c9bf8e0
Jiaqi Wang <jiaqi.wangl@tu-braunschweig.de>
2025-04-11 10:50:55 +0200

    update

--- 02.Software/01_PlatformIO/010_FreeRTOS_Software_Timer/src/main.cpp
+++ 02.Software/01_PlatformIO/010_FreeRTOS_Software_Timer/src/main.cpp
@@ -1,28 +1,74 @@
 #include <Arduino.h>
 
-TimerHandle_t lock;
+#define button_pin 0
+#define gpio_pin 2
+
+TimerHandle_t lock, check;
+
+void GPIOInit_Input(int num)
+{
+  *(volatile unsigned int *)GPIO_ENABLE_REG &= ~(1 << num);
+  Serial.printf("IO %d is init", num);
+  delay(10);
+}
+
+void GPIOInit_Output(int num)
+{
+  *(volatile unsigned int *)GPIO_ENABLE_REG |= (1 << num);
+  *(volatile unsigned int *)GPIO_OUT_REG |= (1 << num);
+  Serial.printf("IO %d is init", num);
+  delay(10);
+}
+
+void lockTask(void *param)
+{
+  GPIOInit_Input(button_pin);
+  while (1)
+  {
+    uint32_t val = *(volatile unsigned int *)GPIO_IN_REG;
+
+    if (!(val & (1 << button_pin)))
+    {
+      Serial.printf("GPIO %d = LOW \r\n", button_pin);
+
+      if (xTimerStart(lock, 3000) == pdPASS)
+      {
+        Serial.println("finish");
+      }
+      vTaskDelay(500);
+    }
+    vTaskDelay(10);
+  }
+}
+
+void blinkTask(void *param)
+{
+  GPIOInit_Output(gpio_pin);
+  while (1)
+  {
+    *(volatile unsigned int *)GPIO_OUT_W1TS_REG |= (1 << gpio_pin);
+    vTaskDelay(10);
+  }
+}
+
+void checkCallBack(TimerHandle_t th)
+{
+  Serial.println("checked");
+}
 
 void lockCallBack(TimerHandle_t th)
 {
   Serial.println("locked");
 }
 
-void lockTask(void *param)
-{
-  while (1)
-  {
-    if(xTimerStart(lock, 3000) ==pdPASS) {
-      Serial.println("finish");
-    }
-    vTaskDelay(1000);
-  }
-}
-
 void setup()
 {
   Serial.begin(115200);
-  lock = xTimerCreate("lock the car", 2000, pdFALSE, (void *)0, lockCallBack);
-  xTaskCreate(lockTask, "lock", 1024, NULL, 1, NULL);
+  lock = xTimerCreate("lock the car", 8000, pdFALSE, (void *)0, lockCallBack);
+  check = xTimerCreate("check the led", 2000, pdTRUE, (void *)1, checkCallBack);
+  xTaskCreate(lockTask, "lock", 1024 * 3, NULL, 1, NULL);
+  xTaskCreate(blinkTask, "blink", 1024 * 3, NULL, 1, NULL);
+  xTimerStart(check, portMAX_DELAY);
 }
 
 void loop()
