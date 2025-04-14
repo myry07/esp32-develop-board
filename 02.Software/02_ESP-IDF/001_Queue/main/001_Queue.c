50d6bba5c84045e75fa58bca1bc3842b43f8e8a7
Jiaqi Wang <jiaqi.wangl@tu-braunschweig.de>
2025-04-14 19:22:36 +0200

    update

--- 02.Software/02_ESP-IDF/001_Queue/main/001_Queue.c
+++ 02.Software/02_ESP-IDF/001_Queue/main/001_Queue.c
@@ -23,8 +23,12 @@ void userTask(void *param)
         vTaskDelay(500);
         if (xQueueSend(queueMsg, &d1, timeout) == pdTRUE)
         {
-            ESP_LOGI("User", "sent successfully num: %d", d1.num);
+            ESP_LOGI("User: ", "sent successfully num: %d", d1.num);
             d1.num++;
+        }
+        else
+        {
+            ESP_LOGI("User: ", "sent unsuccessfully");
         }
         vTaskDelay(10);
     }
@@ -38,8 +42,12 @@ void LCDTask(void *param)
     {
         if (xQueueReceive(queueMsg, &d_LCD, timeout) == pdTRUE)
         {
-            ESP_LOGI("LCD", "receive successfully, ID: %d , msg: %s, num: %d",
+            ESP_LOGI("LCD: ", "receive successfully, ID: %d , msg: %s, num: %d",
                      d_LCD.ID, d_LCD.msg, d_LCD.num);
+        }
+        else
+        {
+            ESP_LOGI("LCD: ", "receive unsuccessfully");
         }
         vTaskDelay(10);
     }
@@ -53,7 +61,7 @@ void app_main(void)
     data d2 = {2, "Hello I'm Task2", 0};
     data d3 = {3, "Hello I'm Task3", 0};
 
-    ESP_LOGI("TAG", "begin");
+    ESP_LOGI("Main: ", "begin");
 
     xTaskCreatePinnedToCore(userTask, "User1", 1024 * 3, (void *)&d1, 3, NULL, 1);
     xTaskCreatePinnedToCore(userTask, "User2", 1024 * 3, (void *)&d2, 3, NULL, 1);
